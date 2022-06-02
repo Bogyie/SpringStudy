fdb725e4162f0ce486c38d0e19ef27d141683a82
bogyeong <boggyhint@gmail.com>
2022-06-02 09:33:51 +0900

    fix typing error

--- src/com/spring/ex01/LoggingAdvice.java
+++ src/com/spring/ex01/LoggingAdvice.java
@@ -5,7 +5,7 @@ import org.aopalliance.intercept.MethodInvocation;
 
 public class LoggingAdvice implements MethodInterceptor {
 	public Object invoke(MethodInvocation invocation) throws Throwable {
-		System.out.println("[메서드 호출 전 : LogginAdvice");
+		System.out.println("[메서드 호출 전 : LoggingAdvice");
 		System.out.println(invocation.getMethod() + "메서드 호출 전");
 
 		Object object = invocation.proceed();
